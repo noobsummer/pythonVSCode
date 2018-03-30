eba20785d1d790427632dd8b4cc33a08e737c880
Don Jayamanne <don.jayamanne@yahoo.com>
2018-03-29 20:33:43 -0700

    add logging

--- src/test/pythonFiles/debugging/multiThread.py
+++ src/test/pythonFiles/debugging/multiThread.py
@@ -1,14 +1,20 @@
+print("step1")
 import sys
 import threading
 import time
+print("step2")
 
 def bar():
+    print("step2-loop")
     time.sleep(2)
     print("abcdef")
 
 def foo(x):
     while True:
+        print("step2-while-loop")
         bar()
-
-threading.Thread(target = lambda: foo(2), name="foo").start()
+print("step3")
+threading.Thread(target=lambda: foo(2), name="foo").start()
+print("step4")
 foo(1)
+print("step5")
