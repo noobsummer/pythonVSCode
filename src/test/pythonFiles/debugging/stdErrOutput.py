c2fb6f36735aa7f18014667b6c4c9cce08816516
Don Jayamanne <don.jayamanne@yahoo.com>
2018-04-04 14:18:26 -0700

    add a minor delay for output to be sent

--- src/test/pythonFiles/debugging/stdErrOutput.py
+++ src/test/pythonFiles/debugging/stdErrOutput.py
@@ -1,3 +1,6 @@
 import sys
+import time
+time.sleep(1)
 sys.stderr.write('error output')
 sys.stderr.flush()
+time.sleep(1)
--- src/test/pythonFiles/debugging/stdOutOutput.py
+++ src/test/pythonFiles/debugging/stdOutOutput.py
@@ -1,3 +1,6 @@
 import sys
+import time
+time.sleep(1)
 sys.stdout.write('normal output')
 sys.stdout.flush()
+time.sleep(1)
