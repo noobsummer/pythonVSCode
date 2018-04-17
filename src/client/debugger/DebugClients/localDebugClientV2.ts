16b6a50863057a75554b8a2dcf2faa2d158aa703
Don Jayamanne <don.jayamanne@yahoo.com>
2018-04-17 13:15:41 -0700

    Fixed merge issue

--- src/client/debugger/DebugClients/localDebugClientV2.ts
+++ src/client/debugger/DebugClients/localDebugClientV2.ts
@@ -13,27 +13,6 @@ export class LocalDebugClientV2 extends LocalDebugClient {
         super(args, debugSession, canLaunchTerminal, launcherScriptProvider);
     }
     protected buildDebugArguments(cwd: string, debugPort: number): string[] {
-<<<<<<< 8020cb03529c53726d3eeb013acde4a63338fb8f
-<<<<<<< 157f392165ac177707587117ecf76faffe5c4062
-=======
->>>>>>> :hammer: refactor and enable launch without debug
-        const noDebugArg = this.args.noDebug ? ['--nodebug'] : [];
-        return ['-m', 'ptvsd', ...noDebugArg, '--host', 'localhost', '--port', debugPort.toString()];
-    }
-    protected buildStandardArguments() {
-        const programArgs = Array.isArray(this.args.args) && this.args.args.length > 0 ? this.args.args : [];
-        if (typeof this.args.module === 'string' && this.args.module.length > 0) {
-            return ['-m', this.args.module, ...programArgs];
-        }
-        if (this.args.program && this.args.program.length > 0) {
-            return ['--file', this.args.program, ...programArgs];
-        }
-        return programArgs;
-<<<<<<< 8020cb03529c53726d3eeb013acde4a63338fb8f
-=======
         return [cwd, debugPort.toString()];
->>>>>>> no debug launcher
-=======
->>>>>>> :hammer: refactor and enable launch without debug
     }
 }
