d5a721b38d54a54bc095018d2fce45960c73bbf6
Don Jayamanne <don.jayamanne@yahoo.com>
2018-04-18 09:21:20 -0700

    Fix

--- src/client/debugger/DebugClients/localDebugClientV2.ts
+++ src/client/debugger/DebugClients/localDebugClientV2.ts
@@ -13,6 +13,16 @@ export class LocalDebugClientV2 extends LocalDebugClient {
         super(args, debugSession, canLaunchTerminal, launcherScriptProvider);
     }
     protected buildDebugArguments(cwd: string, debugPort: number): string[] {
-        return [cwd, debugPort.toString()];
+        return ['-m', 'ptvsd', '--host', 'localhost', '--port', debugPort.toString()];
+    }
+    protected buildStandardArguments() {
+        const programArgs = Array.isArray(this.args.args) && this.args.args.length > 0 ? this.args.args : [];
+        if (typeof this.args.module === 'string' && this.args.module.length > 0) {
+            return ['-m', this.args.module, ...programArgs];
+        }
+        if (this.args.program && this.args.program.length > 0) {
+            return ['--file', this.args.program, ...programArgs];
+        }
+        return programArgs;
     }
 }
