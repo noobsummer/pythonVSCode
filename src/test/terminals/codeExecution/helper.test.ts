2f1f9735ba778f2e8b22319b85b3d6e9fdd7ae2f
Don Jayamanne <dojayama@microsoft.com>
2018-05-01 16:17:02 -0700

    More fixes

--- src/test/terminals/codeExecution/helper.test.ts
+++ src/test/terminals/codeExecution/helper.test.ts
@@ -64,7 +64,7 @@ suite('Terminal - Code Execution Helper', () => {
             });
         const normalizedZCode = await helper.normalizeLines(source);
         // In case file has been saved with different line endings.
-        expectedSource = expectedSource.splitLines().join(EOL) + EOL;
+        expectedSource = expectedSource.splitLines({ removeEmptyEntries: false, trim: false }).join(EOL);
         expect(normalizedZCode).to.be.equal(expectedSource);
     }
     test('Ensure blank lines are NOT removed when code is not indented (simple)', async () => {
